e986f2453e51359c8420c3846993db85c8275ef0
jal9626 <jal9626@gmail.com>
2019-05-16 23:59:25 -0400

    debugging

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const server = require('./server.js');
 
 const PORT = process.env.PORT || 4000;
 
-server.listen(4000, () => {
+// const host = '0.0.0.0';
+// const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, () => {
     console.log(`\n* Server Running on PORT ${PORT} *\n`);
 });
