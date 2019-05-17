837553ac22d665cfd11340b46866178d5a88f27f
jal9626 <jal9626@gmail.com>
2019-05-16 22:00:22 -0400

    added env file

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 // code away!
+require('dotenv').config();
+console.log('process env', process.env);
+
 const server = require('./server.js');
 
+const PORT = process.env.PORT || 4000;
+
 server.listen(4000, () => {
-    console.log('\n* Server Running on http:localhost:4000 *\n');
+    console.log(`\n* Server Running on PORT ${PORT} *\n`);
 });
